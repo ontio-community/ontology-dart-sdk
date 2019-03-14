831e72be113c50383da1943a3aac5d8407fdbc79
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2019-03-14 14:40:48 +0800

    catch platform exceptions

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,13 +4,13 @@ ROOT="$(cd "$(dirname "$0")" && pwd)"/../
 
 # clean build cache
 rm -rf ~/Library/Developer/Xcode/DerivedData/Runner-*/Build/
-cd $ROOT/example/ios/
+cd $ROOT/example/
 # should to recreate .xcworkspace file after cleans build
 pod install > /dev/null 2>&1
 flutter build ios
-cd $ROOT
 
 # run tests
+cd $ROOT
 set -o pipefail && \
   xcodebuild \
     -workspace example/ios/Runner.xcworkspace \
@@ -18,3 +18,5 @@ set -o pipefail && \
     -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=12.1' \
     test | xcpretty --test --color 
+
+cd example/ && flutter drive --target=test_driver/app.dart
